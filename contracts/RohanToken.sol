cdd333b0967572a3b9730ebaf41ba20082e8ed05
ＳＡＶＩＴＡＲ <81708317+LE1DENFROST@users.noreply.github.com>
2024-09-25 15:05:35 +0300

    Update RohanToken.sol

--- contracts/RohanToken.sol
+++ contracts/RohanToken.sol
@@ -5,8 +5,8 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract RohanToken is ERC20, Ownable {
-    constructor(address initialOwner) ERC20("ROHAN", "RHA") Ownable(initialOwner) {
-        // Total supply olarak 10 milyon token mint ediliyor
+    constructor(address initialOwner) ERC20("write your own token name", "write your own symbol") Ownable(initialOwner) {
+        // Minting 10 million tokens as total supply
         _mint(msg.sender, 10_000_000 * 10 ** decimals());
     }
 }
