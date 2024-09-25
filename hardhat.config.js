87251e6d0ecd2ebf9c3b5ccd818d5a0879d7a6c4
ＳＡＶＩＴＡＲ <81708317+LE1DENFROST@users.noreply.github.com>
2024-09-25 15:18:35 +0300

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,11 +5,11 @@ module.exports = {
   solidity: "0.8.24",
   networks: {
     sepolia: {
-      url: "https://sepolia.infura.io/v3/c35decbfada24d1498e166a9e4170c9b", // Alchemy veya Infura API key
-      accounts: [`0x${"4776901c3336f1d87e0f0bd51106fe1524e3350b4fc06b63c89f1356102dd08b"}`], // Cüzdan özel anahtarın
+      url: "", // Alchemy or Infura API key
+      accounts: [`0x${"---"}`], // Wallet private key
     },
   },
   etherscan: {
-    apiKey: "R4VHNWZ8W8EMYCRWYSR9NFGHCJF97BC2TD", // Etherscan API key
+    apiKey: "----", // Etherscan API key
   },
 };
